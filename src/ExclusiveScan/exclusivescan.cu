4060b7121753ee1affca9d9fcd966b4b1762e12c
tywerbicki <ty.werbicki@gmail.com>
2023-12-13 22:51:37 -0800

    Kernel clean up. Eval usage of dynamic parallelism

--- src/ExclusiveScan/exclusivescan.cu
+++ src/ExclusiveScan/exclusivescan.cu
@@ -10,33 +10,26 @@ namespace cg = cooperative_groups;
 
 namespace
 {
-    constexpr unsigned int WarpSize         = 4;
-    constexpr unsigned int MaxBlockSize     = 16;
-    constexpr unsigned int MaxWarpsPerBlock = MaxBlockSize / WarpSize;
-
-    // This assumption is implictly made in kernels below.
-    static_assert(MaxWarpsPerBlock <= WarpSize);
+    constexpr unsigned int WarpSize = 4;
 
 
-    template<typename TyVal, typename TyFn>
-    __device__ void ExclusiveScan(const cg::thread_block& block, TyVal* const pData, TyFn&& op)
+    template<typename T, typename F>
+    __device__ void ExclusiveScan_block_kernel(const cg::thread_block& block, T* const pData, F&& op)
     {
         // 1. Allocate shared memory to hold intermediate results obtained from each warp in the thread block.
-        // 2. The allocation size is conservative - the exact calculation is:
-        //        block.num_threads() / WarpSize
-        __shared__ TyVal shared[MaxWarpsPerBlock];
+        // 2. The allocation size is conservative - the exact calculation is block.num_threads() / WarpSize.
+        __shared__ T shared[WarpSize];
 
-        // Partition thread block into warps.
         const cg::thread_block_tile<WarpSize, cg::thread_block> warp = cg::tiled_partition<WarpSize, cg::thread_block>(block);
-        // Calculate the location of the thread in the parent thread block.
-        const unsigned int threadIdxInBlock = (warp.meta_group_rank() * warp.num_threads()) + warp.thread_rank();
-        // Load value to be used in the warp-local exclusive scan.
-        const TyVal originalVal = pData[threadIdxInBlock];
 
-        const TyVal scannedVal = cg::exclusive_scan(warp, originalVal, op);
+        // This ensures we can scan the intermediate results with only 1 warp.
+        assert(warp.meta_group_rank() < WarpSize);
 
-        // The highest-ranked thread in the warp stores its scanned value + original value in shared memory
-        // as an intermediate result.
+        const T originalVal = pData[block.thread_rank()];
+        const T scannedVal  = cg::exclusive_scan(warp, originalVal, op);
+
+        printf("Scanned val: %u\n", scannedVal);
+
         if (warp.thread_rank() + 1 == warp.num_threads())
         {
             shared[warp.meta_group_rank()] = scannedVal + originalVal;
@@ -44,7 +37,6 @@ namespace
 
         block.sync();
 
-        // The first warp in the thread block is used to scan the intermediate results.
         if (warp.meta_group_rank() == 0)
         {
             shared[warp.thread_rank()] = cg::exclusive_scan(warp, shared[warp.thread_rank()], op);
@@ -52,14 +44,14 @@ namespace
 
         block.sync();
 
-        // Add the scanned intermediate result to the original scanned value to complete the block-wide scan.
-        pData[threadIdxInBlock] = shared[warp.meta_group_rank()] + scannedVal;
+        pData[block.thread_rank()] = shared[warp.meta_group_rank()] + scannedVal;
     }
 
 
-    __global__ void kernel(unsigned int* const pData)
+    template<typename T>
+    __global__ void ExclusiveScan_Add_grid_kernel(T* const pData)
     {
-        ExclusiveScan(cg::this_thread_block(), pData, cg::plus<unsigned int>());
+        ExclusiveScan_block_kernel(cg::this_thread_block(), pData, cg::plus<unsigned int>());
     }
 }
 
@@ -73,7 +65,7 @@ int main()
 
     cudaMemcpy(pDataDevice, data, sizeof(data), cudaMemcpyHostToDevice);
 
-    kernel<<<1, 12>>>(pDataDevice);
+    ExclusiveScan_Add_grid_kernel<<<1, 12>>>(pDataDevice);
 
     cudaMemcpy(data, pDataDevice, sizeof(data), cudaMemcpyDeviceToHost);
 
