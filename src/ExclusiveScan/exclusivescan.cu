4f37495f16f939d314734f0841ebb18a4c1909da
tywerbicki <ty.werbicki@gmail.com>
2023-12-14 22:34:52 -0800

    Expand exclusive scan to entire grid

--- src/ExclusiveScan/exclusivescan.cu
+++ src/ExclusiveScan/exclusivescan.cu
@@ -14,7 +14,7 @@ namespace
 
 
     template<typename T, typename F>
-    __device__ void ExclusiveScan_block_kernel(const cg::thread_block& block, T* const pData, F&& op)
+    __device__ void ExclusiveScan_block_kernel(const cg::thread_block& block, T* const pData, F&& op, T* const pBlockSum)
     {
         // 1. Allocate shared memory to hold intermediate results obtained from each warp in the thread block.
         // 2. The allocation size is conservative - the exact calculation is block.num_threads() / WarpSize.
@@ -37,6 +37,11 @@ namespace
 
         block.sync();
 
+        if ((block.thread_rank() + 1 == block.num_threads()) && pBlockSum)
+        {
+            *pBlockSum = scannedVal + originalVal;
+        }
+
         if (warp.meta_group_rank() == 0)
         {
             shared[warp.thread_rank()] = cg::exclusive_scan(warp, shared[warp.thread_rank()], op);
@@ -49,9 +54,15 @@ namespace
 
 
     template<typename T>
-    __global__ void ExclusiveScan_Add_grid_kernel(T* const pData)
+    __global__ void ExclusiveScan_Add_grid_kernel(T* const pData, T* const pBlockSums)
     {
-        ExclusiveScan_block_kernel(cg::this_thread_block(), pData, cg::plus<unsigned int>());
+        const cg::grid_group   grid  = cg::this_grid();
+        const cg::thread_block block = cg::this_thread_block();
+
+        ExclusiveScan_block_kernel(block,
+                                   &pData[grid.block_rank() * block.num_threads()],
+                                   cg::plus<T>(),
+                                   pBlockSums ? &pBlockSums[grid.block_rank()] : pBlockSums);
     }
 }
 
@@ -59,17 +70,28 @@ namespace
 int main()
 {
     unsigned int data[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    unsigned int blockSums[3];
 
     unsigned int* pDataDevice = nullptr;
+    unsigned int* pBlockSums  = nullptr;
     cudaMalloc(&pDataDevice, sizeof(data));
+    cudaMalloc(&pBlockSums, sizeof(blockSums));
 
     cudaMemcpy(pDataDevice, data, sizeof(data), cudaMemcpyHostToDevice);
 
-    ExclusiveScan_Add_grid_kernel<<<1, 12>>>(pDataDevice);
+    ExclusiveScan_Add_grid_kernel<<<3, 4>>>(pDataDevice, pBlockSums);
+    ExclusiveScan_Add_grid_kernel<<<1, 4>>>(pBlockSums, (unsigned int*)0);
 
     cudaMemcpy(data, pDataDevice, sizeof(data), cudaMemcpyDeviceToHost);
+    cudaMemcpy(blockSums, pBlockSums, sizeof(blockSums), cudaMemcpyDeviceToHost);
 
     for (const auto i : data)
+    {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    for (const auto i : blockSums)
     {
         std::cout << i << " ";
     }
